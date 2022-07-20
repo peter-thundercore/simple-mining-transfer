30c6bd40fc427eb862b66d5f4f78e192c1b84ce6
peter <peter@thundercore.tw>
2022-07-20 13:47:58 +0800

    add jekyll

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   assetPrefix: ".",
-  assetPrefix: isProd
-    ? // ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
-      "https://cdn.statically.io/gh/peter-thundercore/simple-mining-transfer/gh-pages/"
-    : "",
 };
 
 module.exports = nextConfig;
