20dc46109640bc939977d643af69400273324c6d
peter <peter@thundercore.tw>
2022-07-20 13:27:31 +0800

    add cdn

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   assetPrefix: ".",
+  assetPrefix: isProd
+    ? // ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+      "https://cdn.statically.io/gh/peter-thundercore/simple-mining-transfer/gh-pages/"
+    : "",
 };
 
 module.exports = nextConfig;
